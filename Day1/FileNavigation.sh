429f7fbfbf313eaea2c45c719d7b2e628a7a9585
Aaron Bushell <aaron_bushell@yahoo.com>
2021-05-17 20:20:51 +0100

    update glossary with examples

--- Day1/FileNavigation.sh
+++ Day1/FileNavigation.sh
@@ -3,6 +3,11 @@
 # cd ../ -                  Up one file
 
 # ls list files in directory
+# ls -a ALL FILES INCLUDING HIDDEN .fullstophiddenfiles
+# ls -r REVERSE order list
+# ls - R RECURSIVE list
+# ls -i gives inode
+#ls - l file list long format
 
 # cd / -                    navigate to root
 
@@ -16,4 +21,31 @@
 
 # ls /home/local/FDMGROUP/  take a look in a directory without mocing
 
-# pwd - Print the full filename of the current working directory
+# mkdir -                   make a folder
+# mkdir - p dir1/dir2       make parent(dir1) and child(dir2)
+
+# touch -                   make a file
+
+# cp -                      Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY
+# example cp scenery.jpg /home/username/Pictures
+#  create a copy of scenery.jpg (from your current directory) into the Pictures directory
+
+# mv example: mv file.txt /home/username/Documents
+# To rename files, the Linux command is mv oldname.ext newname.ext
+# mv -                      Rename SOURCE to DEST, or move SOURCE(s) to DIRECTORY
+# example: mv file.txt /home/username/Documents
+# To rename files, the Linux command is mv oldname.ext newname.ext
+
+# rm - remove files or directories
+# example rmdir dir1 will delete empty directory
+# example rmdir -r dir1 will remove directory and contents (recursive flag
+
+#### GLOBBING ####
+
+# * Match Zero or more character
+# ? Match One character
+# [] Match anything in [] for that character position e.g ls /dev/tty[0-9]
+# The - is a rangae seperator and will match that range
+
+#chmod amends read write execute permissions
+
--- Day1/UnixGlossary.sh
+++ Day1/UnixGlossary.sh
@@ -6,3 +6,50 @@
 # pwd - Print the full filename of the current working directory
 
 # tree - Tree is a recursive directory listing program that produces a depth indented listing of files
+
+# pwd - Print the full filename of the current working directory
+
+# cat - Concatenate FILE(s) to standard output 
+# e.g cat file1.txt > file2.txt
+# Writes content to file. If the file2.txt file doesn’t exist, the command will create it. Otherwise, it will overwrite the file.
+# cat file1.txt >> file2.txt
+# appends dats to file
+# cat file1.txt file2.txt > combinedfile.txt
+ # concatenate the contents of file1.txt and file2.txt and write them to a new file combinedfile.txt using the (>) operator
+
+# more - shows content in pages
+# less - shows content and needs a q to quit
+
+# head - shows first 10 lines of a file
+# tail - shows last 10 lines of a file
+
+# | chain commands with pipe
+# cat /etc/passwd | more - chains more command onto the cat(look in) command
+
+# file tests each argument in an attempt to classify it.  There are three sets of tests, performed in this order: filesystem tests, magic tests, and language tests.  The first test that succeeds causes the file type to be printed.
+# file produces an overview of file
+
+# wc - Print  newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified.  A word is a non-zero-length sequence of characters delimited by white space.
+
+# redirection > command creates or overwrites content
+# redirections >> appends content
+
+# echo prints to screen e.g echo "Haunted" - prints console. echo "House" >> file2 append House into file2
+
+# 2> outputs errors to a file - ls /home/ect 2> errorfile
+# Outputs error to nowhere use dev/null ls /home/ect 2> /dev/null
+
+# Output screen output and error to a file. ls -l file1 afile > bfile 2>&1
+
+# | chains command together e.g ls -l | wc -l perform kist and then count the lines
+
+# tee - Copy standard input to each FILE, and also to standard output.
+# date | tee file - displays and puts in a file
+# ls | tee filelist | wc -l - takes list and puts it in a file and counts lines output to screen
+
+# date - Display the current time in the given FORMAT, or set the system date.
+# date +%A - display day
+# date +%T - display time
+# date +%B - display Mmonth
+
+# < takes standard input
